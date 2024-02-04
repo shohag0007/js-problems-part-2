911a0aa5b646c480e0987d0b6e0b3a1c1d2999d5
shohag0007 <merinedrive007@gmail.com>
2024-02-04 19:21:32 +0600

    tim is the boss

--- max3.js
+++ max3.js
@@ -1,3 +1,15 @@
 const jim = 56;
 const tim = 89;
 const kim = 68;
+
+if(jim > tim && jim > kim){
+    console.log('jim is the best');
+}
+
+else if (tim > jim && tim > kim){
+    console.log('tim is the best');
+}
+
+else{
+    console.log('kim is the boss');
+}
